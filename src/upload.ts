0dbfe31c654ca1360d2303928e13451beb68b3cd
Boshen Chen <boshenc@gmail.com>
2017-03-27 13:52:27 +0800

    temporary remove /upload

--- src/upload.ts
+++ src/upload.ts
@@ -1,70 +1,70 @@
-import { Subject } from 'rxjs/Subject'
-import { Subscriber } from 'rxjs/Subscriber'
+// import { Subject } from 'rxjs/Subject'
+// import { Subscriber } from 'rxjs/Subscriber'
 
-import { post } from './post'
+// import { post } from './post'
 
-const noop = (..._: any[]) => {}
+// const noop = (..._: any[]) => {}
 
-interface RequestConfig {
-  headers?: Object
-  body?: any
-  onCreate?: () => void
-  onProgress?: (pe: ProgressEvent) => void
-  onSuccess?: (response?: any) => void
-  onError?: (error?: any) => void
-}
+// interface RequestConfig {
+  // headers?: Object
+  // body?: {}
+  // onCreate?: () => void
+  // onProgress?: (pe: ProgressEvent) => void
+  // onSuccess?: (response?: Aj) => void
+  // onError?: (error?: any) => void
+// }
 
-interface UploadConfig extends RequestConfig {
-  getUploadUrl: () => string
-}
+// interface UploadConfig extends RequestConfig {
+  // getUploadUrl: () => string
+// }
 
-export const upload = (config: UploadConfig) => {
-  const {
-    body,
-    onCreate = noop,
-    onProgress = noop,
-    onSuccess = noop,
-    onError = noop
-  } = config
+// export const upload = (config: UploadConfig) => {
+  // const {
+    // body,
+    // onCreate = noop,
+    // onProgress = noop,
+    // onSuccess = noop,
+    // onError = noop
+  // } = config
 
-  const progressSubscriber = Subscriber.create(
-    onProgress,
-    noop
-  )
+  // const progressSubscriber = Subscriber.create(
+    // onProgress,
+    // noop
+  // )
 
-  const retry$ = new Subject()
+  // const retry$ = new Subject()
 
-  onCreate()
+  // onCreate()
 
-  const subscription = post({
-    url: config.getUploadUrl(),
-    body,
-    headers: config.headers,
-    isStream: true,
-    progressSubscriber
-  })
-    .do(null, (e) => {
-      onError(e)
-    })
-    .retryWhen(() => {
-      return retry$
-    })
-    .subscribe(
-      (response) => {
-        onSuccess(response)
-      }
-    )
+  // const subscription = post({
+    // url: config.getUploadUrl(),
+    // body,
+    // headers: config.headers,
+    // isStream: true,
+    // progressSubscriber
+  // })
+    // .do(null, (e) => {
+      // onError(e)
+    // })
+    // .retryWhen(() => {
+      // return retry$
+    // })
+    // .subscribe(
+      // (response) => {
+        // onSuccess(response)
+      // }
+    // )
 
-  const abort = () => {
-    subscription.unsubscribe()
-  }
+  // const abort = () => {
+    // subscription.unsubscribe()
+  // }
 
-  const retry = () => {
-    retry$.next()
-  }
+  // const retry = () => {
+    // retry$.next()
+  // }
 
-  return {
-    abort,
-    retry
-  }
-}
+  // return {
+    // abort,
+    // retry
+  // }
+// }
--- test/upload.spec.ts
+++ test/upload.spec.ts
@@ -1,126 +1,126 @@
-const chai = require('chai')
-const sinon = require('sinon')
-const sinonChai = require('sinon-chai')
-chai.use(sinonChai)
-const expect = chai.expect
+// const chai = require('chai')
+// const sinon = require('sinon')
+// const sinonChai = require('sinon-chai')
+// chai.use(sinonChai)
+// const expect = chai.expect
 
-import { upload } from '../src/upload'
+// import { upload } from '../src/upload'
 
-describe('upload', () => {
+// describe.skip('upload', () => {
 
-  const url = '/upload'
-  const config = {
-    getUploadUrl: () => url
-  }
+  // const url = '/upload'
+  // const config = {
+    // getUploadUrl: () => url
+  // }
 
-  let server: any
+  // let server: any
 
-  beforeEach(() => {
-    server = sinon.fakeServer.create()
-  })
+  // beforeEach(() => {
+    // server = sinon.fakeServer.create()
+  // })
 
-  afterEach(() => {
-    server.restore()
-  })
+  // afterEach(() => {
+    // server.restore()
+  // })
 
-  it('should upload to url', () => {
-    const response = { response: 'response' }
-    const headers = { Authorization: 'token' }
-    const onSuccess = sinon.spy()
+  // it('should upload to url', () => {
+    // const response = { response: 'response' }
+    // const headers = { Authorization: 'token' }
+    // const onSuccess = sinon.spy()
 
-    server.respondWith('POST', url, [
-      200,
-      { "Content-Type": "application/json" },
-      JSON.stringify(response)
-    ])
+    // server.respondWith('POST', url, [
+      // 200,
+      // { "Content-Type": "application/json" },
+      // JSON.stringify(response)
+    // ])
 
-    upload({ ...config, headers, onSuccess })
+    // upload({ ...config, headers, onSuccess })
 
-    server.respond()
+    // server.respond()
 
-    expect(server.requests.length).to.equal(1)
-    const request = server.requests[0]
+    // expect(server.requests.length).to.equal(1)
+    // const request = server.requests[0]
 
-    expect(request.url).to.equal(url)
-    expect(request.method).to.equal('POST')
-    expect(JSON.stringify(request.requestHeaders)).to.equal(JSON.stringify({
-      'Content-Type': 'application/octet-stream;charset=utf-8',
-      ...headers
-    }))
-    expect(onSuccess).to.be.calledOnce
-    expect(onSuccess).to.be.calledWith(response)
-  })
+    // expect(request.url).to.equal(url)
+    // expect(request.method).to.equal('POST')
+    // expect(JSON.stringify(request.requestHeaders)).to.equal(JSON.stringify({
+      // 'Content-Type': 'application/octet-stream;charset=utf-8',
+      // ...headers
+    // }))
+    // expect(onSuccess).to.be.calledOnce
+    // expect(onSuccess).to.be.calledWith(response)
+  // })
 
-  it('should call onCreate before ajax call', () => {
-    const onSuccess = sinon.spy()
-    const onCreate = sinon.spy()
-    server.respondWith('POST', url, [200, {}, ''])
+  // it('should call onCreate before ajax call', () => {
+    // const onSuccess = sinon.spy()
+    // const onCreate = sinon.spy()
+    // server.respondWith('POST', url, [200, {}, ''])
 
-    upload({ ...config, onCreate, onSuccess })
-    expect(onCreate).to.be.called
-    server.respond()
-    expect(onSuccess).to.be.called
-  })
+    // upload({ ...config, onCreate, onSuccess })
+    // expect(onCreate).to.be.called
+    // server.respond()
+    // expect(onSuccess).to.be.called
+  // })
 
-  it('should call onProgress while in progress', () => {
-    const progress = {
-      total: 100,
-      loaded: 20
-    }
-    const onProgress = sinon.spy()
+  // it('should call onProgress while in progress', () => {
+    // const progress = {
+      // total: 100,
+      // loaded: 20
+    // }
+    // const onProgress = sinon.spy()
 
-    upload({ ...config, onProgress })
+    // upload({ ...config, onProgress })
 
-    server.requests[0].upload.onprogress(progress)
-    server.respond()
+    // server.requests[0].upload.onprogress(progress)
+    // server.respond()
 
-    expect(onProgress).to.be.calledWith(progress)
-  })
+    // expect(onProgress).to.be.calledWith(progress)
+  // })
 
-  it('should call onError when error', () => {
-    const onError = sinon.spy()
-    const onSuccess = sinon.spy()
+  // it('should call onError when error', () => {
+    // const onError = sinon.spy()
+    // const onSuccess = sinon.spy()
 
-    upload({ ...config, onSuccess, onError })
+    // upload({ ...config, onSuccess, onError })
 
-    server.respond()
+    // server.respond()
 
-    expect(onSuccess).not.to.be.called
-    expect(onError).to.be.called
-  })
+    // expect(onSuccess).not.to.be.called
+    // expect(onError).to.be.called
+  // })
 
-  it('should be able to abort', () => {
-    const onSuccess = sinon.spy()
-    const onError = sinon.spy()
-    const { abort } = upload({ ...config, onSuccess, onError })
-    abort()
-    server.respond()
-    expect(onSuccess).not.to.be.called
-    expect(onError).not.to.be.called
-  })
+  // it('should be able to abort', () => {
+    // const onSuccess = sinon.spy()
+    // const onError = sinon.spy()
+    // const { abort } = upload({ ...config, onSuccess, onError })
+    // abort()
+    // server.respond()
+    // expect(onSuccess).not.to.be.called
+    // expect(onError).not.to.be.called
+  // })
 
-  it('should be able to retry after error', () => {
-    const onSuccess = sinon.spy()
-    const onError = sinon.spy()
+  // it('should be able to retry after error', () => {
+    // const onSuccess = sinon.spy()
+    // const onError = sinon.spy()
 
-    const { abort, retry } = upload({ ...config, onSuccess, onError })
+    // const { abort, retry } = upload({ ...config, onSuccess, onError })
 
-    server.respond()
+    // server.respond()
 
-    expect(server.requests.length).to.equal(1)
-    expect(server.requests[0].readyState).to.equal(4)
-    expect(onSuccess).not.to.be.called
-    expect(onError).to.be.calledOnce
+    // expect(server.requests.length).to.equal(1)
+    // expect(server.requests[0].readyState).to.equal(4)
+    // expect(onSuccess).not.to.be.called
+    // expect(onError).to.be.calledOnce
 
-    retry()
+    // retry()
 
-    server.respond()
+    // server.respond()
 
-    expect(server.requests.length).to.equal(2)
-    expect(server.requests[0].readyState).to.equal(4)
-    expect(server.requests[1].readyState).to.equal(4)
-    expect(onSuccess).not.to.be.called
-    expect(onError).to.be.calledTwice
-  })
+    // expect(server.requests.length).to.equal(2)
+    // expect(server.requests[0].readyState).to.equal(4)
+    // expect(server.requests[1].readyState).to.equal(4)
+    // expect(onSuccess).not.to.be.called
+    // expect(onError).to.be.calledTwice
+  // })
 
-})
+// })
