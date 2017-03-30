985d712cc09aaac7542a7c35c98f7ca68be7f3a5
Boshen Chen <boshenc@gmail.com>
2017-03-30 17:07:01 +0800

    fix handleDrop

--- src/handleDrop.ts
+++ src/handleDrop.ts
@@ -1,17 +1,19 @@
 import { Observable } from 'rxjs/Observable'
 import * as FileAPI from 'fileapi'
 
-import 'rxjs/add/operator/concatAll'
-
 export const handleDrop = (dropElement: HTMLElement, onHover?: Function): Observable<File[]> => {
 
   onHover = onHover || (() => {}) // tslint:disable-line
+  const onHoverElem = (hover) => onHover(dropElement, hover)
 
   return Observable.create((observer) => {
-    FileAPI.event.dnd(dropElement, onHover, (files) => {
+    const onDrop = (files) => {
       observer.next(files)
-    })
+    }
+    FileAPI.event.dnd(dropElement, onHoverElem, onDrop)
+    return () => {
+      FileAPI.event.dnd.off(dropElement, onHoverElem, onDrop)
+    }
   })
-  .concatAll()
 
 }
