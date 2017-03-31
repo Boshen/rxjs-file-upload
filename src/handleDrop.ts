345e35babc8a603f9d9e36dc62ea95e7550403d0
Boshen Chen <boshenc@gmail.com>
2017-03-31 12:44:12 +0800

    add onHover type signature to handleDrop

--- src/handleDrop.ts
+++ src/handleDrop.ts
@@ -1,7 +1,10 @@
 import { Observable } from 'rxjs/Observable'
 import * as FileAPI from 'fileapi'
 
-export const handleDrop = (dropElement: HTMLElement, onHover?: Function): Observable<File[]> => {
+export const handleDrop = (
+  dropElement: HTMLElement,
+  onHover?: (dropElement: HTMLElement, hover: boolean) => void
+): Observable<File[]> => {
 
   onHover = onHover || (() => {}) // tslint:disable-line
   const onHoverElem = (hover) => onHover(dropElement, hover)
